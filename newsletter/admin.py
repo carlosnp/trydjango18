7f1a26045e32ab803da23bd1434aa5ea832cabc6
carlosnp <machinery.e@gmail.com>
2019-01-02 19:57:44 -0400

    actualizacion del modelo SingUp

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import SingUp
 
-admin.site.register(SingUp)
+class SingUpAdmin(admin.ModelAdmin):
+	list_display = ('__str__','timestamp','updated')
+	class Meta:
+		model = SingUp
+
+admin.site.register(SingUp, SingUpAdmin)
--- newsletter/models.py
+++ newsletter/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class SingUp(models.Model):
 	email 		= models.EmailField(verbose_name='Email')
 	full_name 	= models.CharField(max_length=200, blank=True, null=True,verbose_name='Nombre Completo')
-	timestamp 	= models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Marca de Tiempo')
+	timestamp 	= models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Fecha de Creación')
 	updated 	= models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name='Fecha de Actualización')
 
 	class Meta:
-		verbose_name='Carta'
-		verbose_name_plural='Cartas'
+		verbose_name='SingUp'
+		verbose_name_plural='SingUps'
 
 	def __str__(self):
 		return self.email
