d4cfb6fe48884e35b80ce2c3c4a78eaa11b67311
carlosnp <machinery.e@gmail.com>
2019-01-02 19:37:54 -0400

    creacion del modelo SingUp en la app newsletter

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class SingUp(models.Model):
+	email 		= models.EmailField(verbose_name='Email')
+	full_name 	= models.CharField(max_length=200, blank=True, null=True,verbose_name='Nombre Completo')
+	timestamp 	= models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Marca de Tiempo')
+	updated 	= models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name='Fecha de Actualización')
+
+	class Meta:
+		verbose_name='Carta'
+		verbose_name_plural='Cartas'
+
+	def __str__(self):
+		return self.email
